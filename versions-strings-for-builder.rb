4bb29a57df61513da41a001edb1116c48c48d15c
Benoit Daloze <eregontp@gmail.com>
2020-11-07 16:55:06 +0100

    Use the new format for ruby-builder version strings

--- versions-strings-for-builder.rb
+++ versions-strings-for-builder.rb
@@ -3,16 +3,14 @@ versions = eval hash
 
 by_minor = versions[:ruby].group_by { |v| v[/^\d\.\d/] }
 
-p by_minor['2.1']
-p by_minor['2.2']
-p by_minor['2.3']
-
-(4..7).each do |minor|
+(1..7).each do |minor|
   p by_minor["2.#{minor}"].map { |v| "ruby-#{v}" }
 end
 
+puts
 p (versions[:truffleruby] - %w[head]).map { |v| "truffleruby-#{v}" }
 
+puts
 p (versions[:jruby] - %w[head]).map { |v| "jruby-#{v}" }
 
 (versions[:jruby] - %w[head]).each do |v|
