bee16c98fdc00437d5ac15ed6317ba2cc537e363
Koichi ITO <koic.ito@gmail.com>
2021-12-31 18:20:26 +0900

    Support Ruby 3.1.0 for Windows
    Follow up to https://github.com/oneclick/rubyinstaller2/issues/255#issuecomment-1003266597.
    
    This PR adds Ruby 3.1.0 to windows-version.js.

--- dist/index.js
+++ dist/index.js
@@ -58992,6 +58992,7 @@ const versions = {
   "3.0.1": "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.0.1-1/rubyinstaller-3.0.1-1-x64.7z",
   "3.0.2": "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.0.2-1/rubyinstaller-3.0.2-1-x64.7z",
   "3.0.3": "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.0.3-1/rubyinstaller-3.0.3-1-x64.7z",
+  "3.1.0": "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.1.0-1/rubyinstaller-3.1.0-1-x64.7z",
   "head": "https://github.com/oneclick/rubyinstaller2/releases/download/rubyinstaller-head/rubyinstaller-head-x64.7z",
   "mingw": "https://github.com/MSP-Greg/ruby-loco/releases/download/ruby-master/ruby-mingw.7z",
   "mswin": "https://github.com/MSP-Greg/ruby-loco/releases/download/ruby-master/ruby-mswin.7z"
