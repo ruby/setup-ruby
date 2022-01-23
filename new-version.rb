32c0569c64d7b50e54f58efe35f42a63b61f3562
Benoit Daloze <eregontp@gmail.com>
2022-01-23 17:30:44 +0100

    Automatically update the latest version in the README

--- new-version.rb
+++ new-version.rb
@@ -9,6 +9,7 @@ end
 
 raise unless version[MATCH]
 
+# Update ruby-builder-versions.json
 file = "#{__dir__}/ruby-builder-versions.json"
 lines = File.readlines(file, chomp: true)
 
@@ -26,9 +27,23 @@ release_line = lines[from..to].find { |line|
 }
 
 if release_line
-  release_line << " #{version.inspect},"
+  append = " #{version.inspect},"
+  release_line << append unless release_line.end_with?(append)
 else
   lines.insert to+1, "    #{version.inspect},"
 end
 
 File.write(file, lines.join("\n") + "\n")
+
+# Update README.md
+file = "#{__dir__}/README.md"
+lines = File.readlines(file)
+engine_line = lines.find { |line| line.start_with?("| `#{engine}`") }
+engine_line.sub!(/(.+ (?:-|until)) (\d+(?:\.\d+)+)/) do
+  if Gem::Version.new(version) > Gem::Version.new($2)
+    "#{$1} #{version}"
+  else
+    $&
+  end
+end
+File.write(file, lines.join)
