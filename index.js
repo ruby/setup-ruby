4f6f9d46804f13fa60c3f459d15037d5d50464bf
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2022-08-03 14:27:32 -0400

    Step 1: explicitly pass deployment

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const inputDefaults = {
   'rubygems': 'default',
   'bundler': 'Gemfile.lock',
   'bundler-cache': 'false',
+  'deployment': 'true',
   'working-directory': '.',
   'cache-version': bundler.DEFAULT_CACHE_VERSION,
 }
@@ -89,7 +90,7 @@ export async function setupRuby(options = {}) {
 
   if (inputs['bundler-cache'] === 'true') {
     await common.measure('bundle install', async () =>
-      bundler.bundleInstall(gemfile, lockFile, platform, engine, version, bundlerVersion, inputs['cache-version']))
+      bundler.bundleInstall(gemfile, lockFile, platform, engine, version, bundlerVersion, inputs['cache-version'], inputs['deployment']))
   }
 
   core.setOutput('ruby-prefix', rubyPrefix)
