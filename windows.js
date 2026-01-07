a42e0d6414368cf1dcf9f1963e98a06c486a8fbf
Benoit Daloze <eregontp@gmail.com>
2026-01-07 15:45:30 +0100

    Define a helper to download to avoid duplication

--- windows.js
+++ windows.js
@@ -7,7 +7,6 @@ const path = require('path')
 const cp = require('child_process')
 const core = require('@actions/core')
 const exec = require('@actions/exec')
-const tc = require('@actions/tool-cache')
 const common = require('./common')
 const rubyInstallerVersions = require('./windows-versions.json')
 const toolchainVersions = require('./windows-toolchain-versions.json')
@@ -81,8 +80,7 @@ export async function install(platform, engine, version) {
 async function downloadAndExtract(engine, version, url, base, rubyPrefix) {
   const downloadPath = await common.measure('Downloading Ruby', async () => {
     console.log(url)
-    const auth = common.inputs.token ? `token ${common.inputs.token}` : undefined
-    return await tc.downloadTool(url, undefined, auth)
+    return await common.download(url)
   })
 
   const extractPath = process.env.RUNNER_TEMP
@@ -114,8 +112,7 @@ export async function installJRubyTools() {
 async function installMSYS2(url, rubyPrefix = process.env.RUNNER_TEMP) {
   const downloadPath = await common.measure('Downloading msys2 build tools', async () => {
     console.log(url)
-    const auth = common.inputs.token ? `token ${common.inputs.token}` : undefined
-    return await tc.downloadTool(url, undefined, auth)
+    return await common.download(url)
   })
 
   const extractPath = path.join(process.env.RUNNER_TEMP, 'msys64')
@@ -162,8 +159,7 @@ async function installMSYS1(url) {
 
   const downloadPath = await common.measure('Downloading msys1 build tools', async () => {
     console.log(url)
-    const auth = common.inputs.token ? `token ${common.inputs.token}` : undefined
-    return await tc.downloadTool(url, undefined, auth)
+    return await common.download(url)
   })
 
   const msys1Path = `${common.drive}:\\DevKit64`
@@ -199,8 +195,7 @@ async function installMSYS1(url) {
 async function installVCPKG(url) {
   const downloadPath = await common.measure('Downloading mswin vcpkg packages', async () => {
     console.log(url)
-    const auth = common.inputs.token ? `token ${common.inputs.token}` : undefined
-    return await tc.downloadTool(url, undefined, auth)
+    return await common.download(url)
   })
 
   const extractPath = process.env.VCPKG_INSTALLATION_ROOT
